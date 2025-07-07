bd2312f82eb14e5bd6a22eee78bff69efe02f16d
EkSheva <katufka13@mail.ru>
2025-07-07 11:06:45 +0300

    Correct mistakes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,23 +16,14 @@ const Header = () => {
             </a>
           </div>
           <nav className="header__nav">
-            <div
-              className="header__pop-user-set pop-user-set"
-              id="user-set-target"
-            >
-              {/* <!-- <a href="">x</a> --> */}
-              <p className="pop-user-set__name">Ivan Ivanov</p>
-              <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-              <div className="pop-user-set__theme">
-                <p>Темная тема</p>
-                <input type="checkbox" className="checkbox" name="checkbox" />
-              </div>
-              <button type="button" className="_hover03">
-                <a href="#popExit">Выйти</a>
-              </button>
-            </div>
+            <button className="header__btn-main-new _hover01" id="btnMainNew">
+              <a href="#popNewCard">Создать новую задачу</a>
+            </button>
+            <a href="#user-set-target" className="header__user _hover02">
+              Ivan Ivanov
+            </a>
+            <PopUser />
           </nav>
-          <PopUser />
         </div>
       </div>
     </header>
