0d33d82b67593090fd01a5b4f12c2a2e2e80b067
EkSheva <katufka13@mail.ru>
2025-07-23 14:42:01 +0300

    Add Card styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,63 +1,60 @@
+import Date from "../Date/Date/Date";
+import {
+  CardBtn,
+  CardBtnDiv,
+  CardContent,
+  CardGroup,
+  CardsCard,
+  CardsItem,
+  CardTheme,
+  CardThemeP,
+  CardTitle,
+} from "./Card.styled";
+
+const topicColors = {
+  "Web Design": {
+    background: "#ffe4c2",
+    color: "#ff6d00",
+  },
+  Reserch: {
+    background: "#b4fdd1",
+    color: "#06b16e",
+  },
+  Copywriting: {
+    background: "#e9d4ff",
+    color: "#9a48f1",
+  },
+};
+
 const Card = ({ item }) => {
-  const topicColors = {
-    "Web Design": "_orange",
-    Reserch: "_green",
-    Copywriting: "_purple",
+  const topicStyle = topicColors[item.topic] || {
+    background: "#94a6be",
+    color: "#ffffff",
   };
-  const topicColor = topicColors[item.topic] || "gray";
+
   return (
-    <div className="cards__item" id={item.id}>
-      <div className="cards__card card">
-        <div className="card__group">
-          <div className={`card__theme ${topicColor}`}>
-            <p className={`${topicColor}`}>{item.topic}</p>
-          </div>
+    <CardsItem id={item.id}>
+      <CardsCard>
+        <CardGroup>
+          <CardTheme $background={topicStyle.background}>
+            <CardThemeP $color={topicStyle.color}>{item.topic}</CardThemeP>
+          </CardTheme>
           <a href="#popBrowse" target="_self">
-            <div className="card__btn">
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
+            <CardBtn>
+              <CardBtnDiv></CardBtnDiv>
+              <CardBtnDiv></CardBtnDiv>
+              <CardBtnDiv></CardBtnDiv>
+            </CardBtn>
           </a>
-        </div>
-        <div className="card__content">
+        </CardGroup>
+        <CardContent>
           <a href="" target="_blank">
-            <h3 className="card__title">{item.title}</h3>
+            <CardTitle>{item.title}</CardTitle>
           </a>
-          <div className="card__date">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="13"
-              height="13"
-              viewBox="0 0 13 13"
-              fill="none"
-            >
-              <g clipPath="url(#clip0_1_415)">
-                <path
-                  d="M10.5625 2.03125H2.4375C1.7644 2.03125 1.21875 2.5769 1.21875 3.25V10.5625C1.21875 11.2356 1.7644 11.7812 2.4375 11.7812H10.5625C11.2356 11.7812 11.7812 11.2356 11.7812 10.5625V3.25C11.7812 2.5769 11.2356 2.03125 10.5625 2.03125Z"
-                  stroke="#94A6BE"
-                  strokeWidth="0.8"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M11.7812 4.0625H1.21875M3.25 1.21875V2.03125V1.21875ZM9.75 1.21875V2.03125V1.21875Z"
-                  stroke="#94A6BE"
-                  strokeWidth="0.8"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_1_415">
-                  <rect width="13" height="13" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
-            <p>30.10.23</p>
-          </div>
-        </div>
-      </div>
-    </div>
+          <Date />
+        </CardContent>
+      </CardsCard>
+    </CardsItem>
   );
 };
 
