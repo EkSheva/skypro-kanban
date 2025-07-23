a3a6d6dcaa57f4a94a55bddfab4a793b18a6818f
EkSheva <katufka13@mail.ru>
2025-07-23 14:42:34 +0300

    Add CardLoader styles

---
+++ src/components/CardLoader/CardLoader.styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const SCardLoader = styled.div`
+  background-color: rgba(255, 255, 255, 1);
+  width: 220px;
+  height: 130px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: stretch;
+  padding: 15px 13px 19px;
+  margin-bottom: 10px;
+`;
+
+export const LCardTheme = styled.div`
+  padding: 15px 14px 15px 0px;
+`;
+export const LCardBtn = styled.div`
+  padding: 2px;
+`;
