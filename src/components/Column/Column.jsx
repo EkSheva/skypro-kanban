0db432b20127bbd821cfba3a9e3aad894a573a1a
EkSheva <katufka13@mail.ru>
2025-08-27 01:52:58 +0300

    Redact loader

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -3,24 +3,26 @@ import Card from "../Card/Card";
 import CardLoader from "../CardLoader/CardLoader";
 import { Cards, ColumnTitle, MainColumn, PTitle } from "./Column.styled";
 import { TasksContext } from "../../context/TasksContext";
+import { CalendarDateEnd } from "../Calendar/Calendar.styled";
 
 const Column = ({ title, loading }) => {
   const { tasks } = useContext(TasksContext);
+  const filteredTasks = tasks.filter((card) => card.status === title);
   return (
     <MainColumn>
       <ColumnTitle>
         <PTitle>{title}</PTitle>
       </ColumnTitle>
       <Cards>
-        {tasks
-          .filter((card) => card.status === title)
-          .map((card) =>
-            loading ? (
-              <CardLoader key={card?._id} />
-            ) : (
-              <Card card={card} key={card?._id} />
-            )
-          )}
+        {loading ? (
+          Array.from({ length: 3 }).map((_, index) => (
+            <CardLoader key={`loader-${index}`} />
+          ))
+        ) : filteredTasks.length > 0 ? (
+          filteredTasks.map((card) => <Card card={card} key={card._id} />)
+        ) : (
+          <CalendarDateEnd>Нет задач</CalendarDateEnd>
+        )}
       </Cards>
     </MainColumn>
   );
